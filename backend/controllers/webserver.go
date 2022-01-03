5123de974566ebd951cb70587b17df6e4514b151
goya5858 <hokuzaki1203@gmail.com>
2022-01-03 15:04:25 +0900

    MySQLへのアクセス確認

--- backend/controllers/webserver.go
+++ backend/controllers/webserver.go
@@ -15,7 +15,7 @@ import (
 
 func connectMySQL() {
 	fmt.Println("Connect MySQL")
-	db, err := sql.Open("mysql", "backend:docker@tcp(mysql:3306)/react_go_app")
+	db, err := sql.Open("mysql", "backend:docker@tcp(mysql_container:3306)/react_go_app")
 	if err != nil {
 		panic(err.Error())
 	}
